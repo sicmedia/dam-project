cb764f394586a820f44a47f13c04635d64434f4e
alelopez07 <alelo.lopez07@gmail.com>
2018-06-14 16:13:48 -0600

    [FIX]

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -7,6 +7,11 @@ use App\Category_client;
 use App\Status_client;
 use App\Client;
 use Illuminate\Http\Request;
+
+use Aws\S3\S3Client;
+use League\Flysystem\AwsS3v3\AwsS3Adapter;
+use League\Flysystem\Filesystem;
+
 use Illuminate\Support\Facades\Storage;
 
 class ClientController extends Controller {
@@ -16,11 +21,30 @@ class ClientController extends Controller {
 
   public function store(Request $request){
 
+    /*
     $fileName = time().'.txt'; // generate unique name.
     $res = Storage::disk('do_spaces')->put($fileName, 'Hi…');
     Storage::disk('do_spaces')->setVisibility($fileName, 'public'); // Set the visibility to public.
     $url = Storage::disk('do_spaces')->url($fileName);
     return Response::json(['success' => true, 'response' => $url]);
+
+
+    $client = new S3Client([
+        'credentials' => [
+            'key'    => 'UQGPQMJKAOHE5PY2HBY5',
+            'secret' => 'aOWa6fR52cnstu/17eZuyPZcOHQhyzzEjv8ZnoWrbtc'
+        ],
+        'region' => 'ny3',
+        'version' => 'latest',
+        'endpoint' => 'https://nyc3.digitaloceanspaces.com',
+    ]);
+
+    $fileName = time().'.txt'; // generate unique name.
+    $adapter = new AwsS3Adapter($client, 'sicmedia-storage');
+    $filesystem = new Filesystem($adapter);*/
+
+    Storage::disk('spaces')->putFile('dam-project/office-gurus', request()->upload_file, 'public');
+
 
   }
 
